32f979b3ba52bbe1c310e8e7c11b210732d65fba
Yuwen Chiu <yuwenchiu@yuwende-macbook-pro.local>
2018-08-26 14:00:42 +0800

    if else練習

--- CoffeenClothes/ViewController.swift
+++ CoffeenClothes/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coffeeTextField: UITextField!
+    @IBOutlet weak var introLabel: UILabel!
+    @IBOutlet weak var outfitImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        if coffeeTextField.text == "拿鐵" {
+            introLabel.text = "偏好拿鐵的人重視衣服的舒適性，喜愛一套就走的基本款。"
+            outfitImageView.image = UIImage(named: "latte")
+        }
+        else if coffeeTextField.text == "黑咖啡" {
+            introLabel.text = "偏好黑咖啡的人重視俐落簡潔感，經常以Tshirt搭配窄褲體現「Less is more」的精髓。"
+            outfitImageView.image = UIImage(named: "black")
+        }
+        else if coffeeTextField.text == "卡布奇諾" {
+            introLabel.text = "偏好卡布奇諾的人重視「這件我可以穿多久」？喜歡歷久不衰的經典款單品，像是風衣、圓點或格子。"
+            outfitImageView.image = UIImage(named: "cappu")
+        }
+        
+    }
+    
 
 }
 
